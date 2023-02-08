c270bf8b3f30338278aefe60e54b3c78f033b7b8
Edgar Bollow <123127896+eBollow05@users.noreply.github.com>
2023-02-08 19:26:58 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -44,16 +44,18 @@ function edg_vcard_dls_example() {
 	$birthdate_res = false;
 	$today = new DateTime();
 	$birthdate_date = '';
-	
+
 	if ( is_numeric( $birthdate ) ) {
 		$birthdate_date = date_i18n( 'Y-m-d', $birthdate );
 		$birthdate_res = date_create( $birthdate_date );
 	} else {
 		$birthdate_res = date_create( $birthdate );
 	}
-	
-	if ( ! $birthdate || ! $birthdate_res || ! $birthdate_res->format( 'Y-m-d' ) || $birthdate_res > $today ) return;
-	
+
+	if ( ! $birthdate || ! $birthdate_res || ! $birthdate_res->format( 'Y-m-d' ) || $birthdate_res > $today ) {
+		$birthdate_res = '';
+	};
+
 	$birthdate_res = $today->diff( $birthdate_res );
 	$birthdate_res = $birthdate_res->format( 'Y-m-d' );
 
